afbdd73cba64107ca0d8f6bdb0c8dcec7d6d2594
hwangJi-dev <melon2228@swu.ac.kr>
2022-09-18 21:00:43 +0900

    RIBs Tutorial 1

---
+++ RIBsTutorial/RIBsTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RIBsTutorial
+//
+//  Created by hwangJi on 2022/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
